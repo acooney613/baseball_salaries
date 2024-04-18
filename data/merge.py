520cc2f2ac5b3d7a21fe3b1bd9d4318ce57106c7
Aidan Cooney <acooney613@gmail.com>
2024-04-17 20:37:43 -0600

    update

--- data/merge.py
+++ data/merge.py
@@ -30,5 +30,30 @@ stats = batter.drop(['Name', 'Age', 'Tm', 'Lg', 'G', 'Pos Summary'], axis = 1)
 baseball = field.merge(salary, on = ['Name-additional', 'Season'])
 baseball = baseball.merge(stats, on = ['Name-additional', 'Season'])
 
+baseball = baseball.drop(['Name', 'Age', 'Name-additional'], axis = 1)
+baseball['Salary'] = baseball['Salary'].str.replace('$', '').astype(float)
+
+baseball['Pos_C'] = baseball['Position'].apply(lambda x: 1 if 'C' in x else 0)
+baseball['Pos_1B'] = baseball['Position'].apply(lambda x: 1 if '1B' in x else 0)
+baseball['Pos_2B'] = baseball['Position'].apply(lambda x: 1 if '2B' in x else 0)
+baseball['Pos_3B'] = baseball['Position'].apply(lambda x: 1 if '3B' in x else 0)
+baseball['Pos_SS'] = baseball['Position'].apply(lambda x: 1 if 'SS' in x else 0)
+baseball['Pos_OF'] = baseball['Position'].apply(lambda x: 1 if 'OF' in x else 0)
+
+baseball['Num_Pos'] = baseball[['Pos_C', 'Pos_1B', 'Pos_2B', 'Pos_3B', 'Pos_SS', 'Pos_OF']].sum(axis = 1)
+baseball['R/AB'] = baseball['R'] / baseball['AB']
+baseball['2B/AB'] = baseball['2B'] / baseball['AB']
+baseball['3B/AB'] = baseball['3B'] / baseball['AB']
+baseball['HR/AB'] = baseball['HR'] / baseball['AB']
+baseball['RBI/AB'] = baseball['RBI'] / baseball['AB']
+baseball['BB/PA'] = baseball['BB'] / baseball['PA']
+baseball['SB - CS'] = baseball['SB'] - baseball['CS']
+baseball['BB - SO'] = baseball['BB'] - baseball['SO'] # measures a batters eye
+baseball['E/Def-Inn'] = baseball['E'] / baseball['Def-Inn']
+baseball['DP/Def-Inn'] = baseball['DP'] / baseball['Def-Inn']
+
+baseball = baseball.drop(['Position', 'Def-Inn', 'PO', 'A', 'E', 'DP', 'PA', 'AB', 'R', 'H', 
+                          '2B', '3B', 'HR', 'RBI', 'SB', 'CS', 'BB', 'SO', 'TB', 'GDP', 'HBP', 'SH', 'SF', 'IBB'], axis = 1)
+
 # Write to CSV
 baseball.to_csv('baseball.csv', index = False)
